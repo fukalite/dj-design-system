61cc5c0164385611e2b8545b2e5baa4f22f7c627
marcelkornblum <me@marcelkornblum.com>
2026-06-16 01:24:02 +0100

    test: Restore missing badge_gallery.py to fix test

---
+++ example_project/demo_components/components/badge_gallery.py
@@ -0,0 +1,11 @@
+from dj_design_system.data import GalleryParameter
+
+basic_kwargs = {
+    "text": "New",
+}
+
+maximal_kwargs = {
+    "text": "Unread Messages",
+    "theme": "danger",
+    "classes": "font-bold",
+}
